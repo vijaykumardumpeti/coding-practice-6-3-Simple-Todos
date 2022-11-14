50a9041da021260d3c1ee284d39f6045d4aebc54
vijaykumardumpeti <vijaydumpeti70@gmail.com>
2022-11-14 07:17:05 +0000

    Simple Todos

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import SimpleTodos from './components/SimpleTodos'
+
+import './App.css'
+
+const App = () => (
+  <div className="bg-container">
+    <div className="container">
+      <SimpleTodos />
+    </div>
+  </div>
+)
+
+export default App
---
+++ src/components/TodoItem/index.js
@@ -0,0 +1,31 @@
+// Write your code here
+
+import {Component} from 'react'
+
+import './index.css'
+
+class TodoItem extends Component {
+  render() {
+    const {eachObject, key, onDelete} = this.props
+
+    const {id, title} = eachObject
+
+    const deleteFunction = () => {
+      onDelete(id)
+    }
+
+    return (
+      <li key={key} className="line">
+        <div>
+          <p className="para">{title}</p>
+        </div>
+        <div>
+          <button onClick={deleteFunction} className="button" type="button">
+            Delete
+          </button>
+        </div>
+      </li>
+    )
+  }
+}
+export default TodoItem
